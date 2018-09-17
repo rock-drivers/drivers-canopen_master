3ead26518628d4b5f94dac3479960837d8124621
Sylvain Joyeux <sylvain.joyeux@13robotics.com>
2018-09-17 12:32:11 -0300

    set the time on the Sync message
    This allows other CANopen modules to use the timestamp as their
    time synchronization

--- src/StateMachine.cpp
+++ src/StateMachine.cpp
@@ -212,6 +212,7 @@ base::Time StateMachine::timestamp(uint16_t objectId, uint8_t subId) const
 canbus::Message StateMachine::sync()
 {
     canbus::Message msg;
+    msg.time = base::Time::now();
     msg.can_id = 0x80;
     msg.size = 0;
     return msg;
